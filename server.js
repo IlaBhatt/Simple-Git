edfe3d7a9ff84964af095c255570f20760d431e5
ila <ilabhatt2000@gmail.com>
2022-11-24 14:39:36 +0530

    changes done

---
+++ server.js
@@ -0,0 +1,41 @@
+// hello
+const shell = require("shelljs");
+// shell.exec('./feature')
+const { exec } = require("node:child_process");
+exec('git add . \n git commit -m "hi" \n git push', (error, stdout, stderr) => {
+  if (error) {
+    console.log(error);
+  }
+  if (stderr) {
+    console.log(stderr);
+  }
+  console.log(stdout);
+});
+// exec(
+//   'git merge feature',
+//   (error, stdout, stderr) => {
+//     if (error) {
+//       console.log(error);
+//     }
+//     if (stderr) {
+//       console.log(stderr);
+//     }
+//     console.log(stdout);
+//   }
+// );
+// let st=''
+// exec(
+//  "git diff --name-only ..origin/feature",
+//   (error, stdout, stderr) => {
+//     if (error) {
+//       console.log(error);
+//     }
+//     if (stderr) {
+//       console.log(stderr);
+//     }
+//     st=stdout
+//     console.log('st',st);
+//     console.log(stdout);
+//   }
+// );
+// console.log(st);
