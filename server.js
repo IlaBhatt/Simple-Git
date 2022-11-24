9a31f37eed0db97a32f9ba8c206f4ec62b1cfbce
ila <ilabhatt2000@gmail.com>
2022-11-24 15:07:22 +0530

    changes done

--- server.js
+++ server.js
@@ -3,15 +3,18 @@ const shell = require("shelljs");
 // shell.exec('./feature')
 const { exec } = require("node:child_process");
 // \n git commit -m \"hi\" \n git push --set-upstream origin main
-exec("git checkout feature", (error, stdout, stderr) => {
-  if (error) {
-    console.log(error);
+exec(
+  'git checkout main \n git add . \n git commit -m "hi" \n git push --set-upstream origin feature',
+  (error, stdout, stderr) => {
+    if (error) {
+      console.log(error);
+    }
+    if (stderr) {
+      console.log(stderr);
+    }
+    console.log(stdout);
   }
-  if (stderr) {
-    console.log(stderr);
-  }
-  console.log(stdout);
-});
+);
 // exec(
 //   'git merge feature',
 //   (error, stdout, stderr) => {
