4dba0313edce9663ed4872ef10b0a78a356c3d3a
ila <ilabhatt2000@gmail.com>
2022-11-24 15:03:38 +0530

    Changes

---
+++ server.js
@@ -0,0 +1,42 @@
+// hello
+const shell = require("shelljs");
+// shell.exec('./feature')
+const { exec } = require("node:child_process");
+// \n git commit -m \"hi\" \n git push --set-upstream origin main
+exec("git checkout feature", (error, stdout, stderr) => {
+  if (error) {
+    console.log(error);
+  }
+  if (stderr) {
+    console.log(stderr);
+  }
+  console.log(stdout);
+});
+// exec(
+//   'git merge feature',
+//   (error, stdout, stderr) => {
+//     if (error) {
+//       console.log(error);
+//     }
+//     if (stderr) {
+//       console.log(stderr);
+//     }
+//     console.log(stdout);
+//   }
+// );
+// let st=''
+// exec(
+//  "git diff --name-only ..origin/feature",
+//   (error, stdout, stderr) => {
+//     if (error) {
+//       console.log(error);
+//     }
+//     if (stderr) {
+//       console.log(stderr);
+//     }
+//     st=stdout
+//     console.log('st',st);
+//     console.log(stdout);
+//   }
+// );
+// console.log(st);
