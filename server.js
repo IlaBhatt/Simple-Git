c6ad81cd4a821f07f2f35efae5555cc2ea390211
ila <ilabhatt2000@gmail.com>
2022-11-24 16:18:01 +0530

    hi

--- server.js
+++ server.js
@@ -4,16 +4,51 @@ const shell = require("shelljs");
 const { exec } = require("node:child_process");
 // \n git commit -m \"hi\" \n git push --set-upstream origin main
 // \n git commit -m "h1" \n git checkout feature \n git add . \n git commit -m "hi" \n git push --set-upstream origin feature
-exec("git add .", (error, stdout, stderr) => {
-  if (error) {
-    console.log(error);
-  }
-  if (stderr) {
-    console.log(stderr);
-  }
-  console.log(stdout);
-});
+async function gitAdd() {
+  exec("git add .", (error, stdout, stderr) => {
+    if (error) {
+      console.log(error);
+    }
+    if (stderr) {
+      console.log(stderr);
+    }
+    console.log(stdout);
+  });
+}
+
+async function gitCommit() {
+  exec('git commit -m "hi"', (error, stdout, stderr) => {
+    if (error) {
+      console.log(error);
+    }
+    if (stderr) {
+      console.log(stderr);
+    }
+    console.log(stdout);
+  });
+}
+
+async function gitCheckout() {
+  exec("git checkout feature", (error, stdout, stderr) => {
+    if (error) {
+      console.log(error);
+    }
+    if (stderr) {
+      console.log(stderr);
+    }
+    console.log(stdout);
+  });
+}
+
+async function f1() {
+  await gitAdd();
+  await gitCommit();
+  await gitCheckout();
+}
+
+f1();
 // exec(
+
 //   'git merge feature',
 //   (error, stdout, stderr) => {
 //     if (error) {
